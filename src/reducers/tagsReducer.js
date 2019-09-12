979a0810123bda6567f052c18c0a0eda44433c75
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-12 16:27:59 -0700

    added a tagsReducer, created inital state and returned it, and hooked reducer into combineReducer

---
+++ src/reducers/tagsReducer.js
@@ -0,0 +1,8 @@
+const initialState = [{ name: '', color: '' }];
+
+export default function reducer(state = initialState, action) {
+  switch(action.type) {
+    default:
+      return state;
+  }
+}
