30a255b9add7919164ea22318d6dfbeb50cd9925
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-12 16:33:10 -0700

    added promise mddleware redux and added it to store.js

--- src/store.js
+++ src/store.js
@@ -1,9 +1,14 @@
-import { createStore } from 'redux';
+import { createStore, compose, applyMiddleware } from 'redux';
 import reducer from './reducers';
+import { promiseMiddleware } from 'promise-middleware-redux';
+
+const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
 
 const store = createStore(
-  reducer, 
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  reducer,
+  composeEnhancers(applyMiddleware(
+    promiseMiddleware
+  ))
 );
 
 export default store;
