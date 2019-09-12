6906e86bc6b3e3c4f184c721f49592d5990eef93
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-12 15:45:05 -0700

    added a placeholder image and rendered the avatar photo in app for now using it

--- src/components/App.js
+++ src/components/App.js
@@ -3,11 +3,14 @@ import Header from './header/Header';
 import SearchBar from './searchBar/SearchBar';
 import NewsList from './news/NewsList';
 import TagsList from './tags/TagsList';
+import Avatar from '../components/avatar/Avatar';
+import placeholder from '../assets/placeholder.png';
 
 export default function App() {
   return (
     <>
       <Header />
+      <Avatar image={placeholder} />
       <SearchBar />
       <NewsList
         news={[
