842266da41ccab147215727789e06071327b8d2d
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-12 17:17:58 -0700

    created tagsAction and implemented in tagsReducer. Holding off on testing until API is ready. Created getTags selector

--- src/reducers/tagsReducer.js
+++ src/reducers/tagsReducer.js
@@ -1,7 +1,11 @@
+import { FETCH_TAGS } from '../actions/tagsActions';
+
 const initialState = [{ name: '', color: '' }];
 
 export default function reducer(state = initialState, action) {
   switch(action.type) {
+    case FETCH_TAGS:
+      return { state: action.payload };
     default:
       return state;
   }
