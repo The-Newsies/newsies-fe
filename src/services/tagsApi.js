2fddffef44d6a9653d6712efbda22fac55d5a599
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-13 11:09:58 -0700

    added token to our service for Auth0 and ammended our getTags request to include it in the header

--- src/services/tagsApi.js
+++ src/services/tagsApi.js
@@ -1,14 +1,28 @@
-export const getTags = () => {
-  return fetch('URL WILL GO HERE')
-    .then(res => {
-      if(!res.ok) throw 'Unable to retrieve tags';
+let token = null;
+export const setToken = newToken => {
+  token = newToken;
+};
 
-      return res.json();
-    })
-    .then(tags => {
-      return tags.map(tag => ({
-        name: tag.name,
-        color: tag.color
-      }));
-    });
+export const getTags = () => {
+  return (
+    fetch('URL WILL GO HERE'),
+    {
+      method: 'GET',
+      headers: {
+        'Content-Type': 'application/json',
+        Authorization: `Bearer ${token}`
+      }
+    }
+      .then(res => {
+        if(!res.ok) throw 'Unable to retrieve tags';
+
+        return res.json();
+      })
+      .then(tags => {
+        return tags.map(tag => ({
+          name: tag.name,
+          color: tag.color
+        }));
+      })
+  );
 };
