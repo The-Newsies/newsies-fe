4dd0535319b62b3a9c3ad93a2d632d47eef32280
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-15 16:46:02 -0700

    added our logo into our assets folder, plugged it into our header instead of title and gave it a rough size that will most likely be updated

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -3,13 +3,14 @@ import { useAuth0 } from '../../Auth0Provider';
 import LandingWrapper from './LandingWrapper';
 import MyViewWrapper from './MyViewWrapper';
 import styles from './Header.css';
+import logo from '../../assets/logo.png';
 
 export default function Header() {
   const { isAuthenticated } = useAuth0();
   return (
     <>
     <div className={styles.header}>
-      <h1>Newzie</h1>
+      <img className={styles.logo} src={logo} />
       {!isAuthenticated ? <LandingWrapper /> : <MyViewWrapper />}
     </div>
     <hr className={styles.line} />
