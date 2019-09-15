cc291887431ffd6ba128f30abc9c26fb12e19b41
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-15 13:21:10 -0700

    added a CSS file for Avatar and styled the size and border of the image to match our wireframe

--- src/components/avatar/Avatar.js
+++ src/components/avatar/Avatar.js
@@ -1,13 +1,14 @@
 import React from 'react';
 import { useAuth0 } from '../../Auth0Provider';
 import avatar from '../../assets/earth.png';
+import styles from './Avatar.css';
 
 export default function Avatar() {
   const { user } = useAuth0();
   return (
     <div>
-      {user ? <img src={user.picture} /> 
-        : <img src={avatar} />}
+      {user ? <img className={styles.avatarImg} src={user.picture} /> 
+        : <img className={styles.avatarImg} src={avatar} />}
     </div>
   );
 }
