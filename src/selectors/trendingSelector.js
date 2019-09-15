ec922343e736107c65f6815576db02d5354f7c23
sepuckett86 <sepuckett86@gmail.com>
2019-09-15 16:25:35 -0700

    fixed selector and tests for trending

--- src/selectors/trendingSelector.js
+++ src/selectors/trendingSelector.js
@@ -1,2 +1,2 @@
-export const getTrending = state => state.articles;
-export const getLoading = state => state.loading;
+export const getTrending = state => state.trending.articles;
+export const getLoading = state => state.trending.loading;
--- src/selectors/trendingSelector.test.js
+++ src/selectors/trendingSelector.test.js
@@ -2,23 +2,25 @@ import { getTrending, getLoading } from './trendingSelector';
 
 describe('trendingSelector tests', () => {
   it('gets articles from state', () => {
-    const state = {
-      articles: [
-        {
-          source: {
-            id: null,
-            name: 'Nypost.com'
-          },
-          author: 'Jackie Salo',
-          title: 'Vaping mom left in coma from mystery lung illness - New York Post ',
-          description: 'A Texas mom of three says that her vaping habit was to blame for a mysterious lung illness that left her in a coma. Sherie Canada of Abilene took to social media after a 24-day hospital stint to wa…',
-          url: 'https://nypost.com/2019/09/15/vaping-mom-left-in-coma-from-mystery-lung-illness/',
-          urlToImage: 'https://thenypost.files.wordpress.com/2019/09/canada-mom-coma.jpg?quality=90&strip=all&w=1200',
-          publishedAt: '2019-09-15T20:08:00Z',
-          content: 'A Texas mom of three says that her vaping habit was to blame for a mysterious lung illness that left her in a coma.\r\nSherie Canada of Abilene took to social media after a 24-day hospital stint to warn others that e-cigarettes “nearly killed” her.\r\n“You have n… [+1636 chars]'
-        }
-      ],
-      loading: false
+    const state = { 
+      trending: {
+        articles: [
+          {
+            source: {
+              id: null,
+              name: 'Nypost.com'
+            },
+            author: 'Jackie Salo',
+            title: 'Vaping mom left in coma from mystery lung illness - New York Post ',
+            description: 'A Texas mom of three says that her vaping habit was to blame for a mysterious lung illness that left her in a coma. Sherie Canada of Abilene took to social media after a 24-day hospital stint to wa…',
+            url: 'https://nypost.com/2019/09/15/vaping-mom-left-in-coma-from-mystery-lung-illness/',
+            urlToImage: 'https://thenypost.files.wordpress.com/2019/09/canada-mom-coma.jpg?quality=90&strip=all&w=1200',
+            publishedAt: '2019-09-15T20:08:00Z',
+            content: 'A Texas mom of three says that her vaping habit was to blame for a mysterious lung illness that left her in a coma.\r\nSherie Canada of Abilene took to social media after a 24-day hospital stint to warn others that e-cigarettes “nearly killed” her.\r\n“You have n… [+1636 chars]'
+          }
+        ],
+        loading: false
+      }
     };
 
     const trendingArticles = getTrending(state);
@@ -41,23 +43,25 @@ describe('trendingSelector tests', () => {
   });
 
   it('gets loading from state', () => {
-    const state = {
-      articles: [
-        {
-          source: {
-            id: null,
-            name: 'Nypost.com'
-          },
-          author: 'Jackie Salo',
-          title: 'Vaping mom left in coma from mystery lung illness - New York Post ',
-          description: 'A Texas mom of three says that her vaping habit was to blame for a mysterious lung illness that left her in a coma. Sherie Canada of Abilene took to social media after a 24-day hospital stint to wa…',
-          url: 'https://nypost.com/2019/09/15/vaping-mom-left-in-coma-from-mystery-lung-illness/',
-          urlToImage: 'https://thenypost.files.wordpress.com/2019/09/canada-mom-coma.jpg?quality=90&strip=all&w=1200',
-          publishedAt: '2019-09-15T20:08:00Z',
-          content: 'A Texas mom of three says that her vaping habit was to blame for a mysterious lung illness that left her in a coma.\r\nSherie Canada of Abilene took to social media after a 24-day hospital stint to warn others that e-cigarettes “nearly killed” her.\r\n“You have n… [+1636 chars]'
-        }
-      ],
-      loading: false
+    const state = { 
+      trending: {
+        articles: [
+          {
+            source: {
+              id: null,
+              name: 'Nypost.com'
+            },
+            author: 'Jackie Salo',
+            title: 'Vaping mom left in coma from mystery lung illness - New York Post ',
+            description: 'A Texas mom of three says that her vaping habit was to blame for a mysterious lung illness that left her in a coma. Sherie Canada of Abilene took to social media after a 24-day hospital stint to wa…',
+            url: 'https://nypost.com/2019/09/15/vaping-mom-left-in-coma-from-mystery-lung-illness/',
+            urlToImage: 'https://thenypost.files.wordpress.com/2019/09/canada-mom-coma.jpg?quality=90&strip=all&w=1200',
+            publishedAt: '2019-09-15T20:08:00Z',
+            content: 'A Texas mom of three says that her vaping habit was to blame for a mysterious lung illness that left her in a coma.\r\nSherie Canada of Abilene took to social media after a 24-day hospital stint to warn others that e-cigarettes “nearly killed” her.\r\n“You have n… [+1636 chars]'
+          }
+        ],
+        loading: false
+      }
     };
 
     const loading = getLoading(state);
