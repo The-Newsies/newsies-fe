67827671a1be3ef4cf548a37664590edbb6cfe29
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-16 14:41:45 -0700

    added Trending link to landing page

--- src/components/header/LandingWrapper.js
+++ src/components/header/LandingWrapper.js
@@ -2,6 +2,7 @@ import React from 'react';
 import SearchBar from '../searchBar/SearchBar';
 import { useAuth0 } from '../../Auth0Provider';
 import styles from './LandingWrapper.css';
+import { NavLink } from 'react-router-dom';
 
 export default function LandingWrapper() {
   const { auth0Client } = useAuth0();
@@ -11,6 +12,7 @@ export default function LandingWrapper() {
     <div className={styles.container}>
       <SearchBar />
       <button onClick={login}>Sign-in | Sign-up</button>
+      <NavLink to='/'>Trending</NavLink>
     </div>
   );
 }
