d049cc83a99903d37093215d62fd094c449b5326
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-16 12:56:18 -0700

    added bare-bones styling to take the underline away from the link, also updated snapshots

--- src/components/newsView/NewsViewToggle.js
+++ src/components/newsView/NewsViewToggle.js
@@ -1,11 +1,20 @@
 import React from 'react';
 import { NavLink } from 'react-router-dom';
+import styles from './NewsViewToggle.css';
 
 export default function NewsViewToggle() {
   return (
     <span>
-      <NavLink to='/trending' activeStyle={{ fontWeight: 'bold', color: 'white', background: 'red' }}>Trending</NavLink>
-      <NavLink to='/myNews' activeStyle={{ fontWeight: 'bold', color: 'white', background: 'red' }}>My News</NavLink>
+      <NavLink to='/trending' className={styles.link} activeStyle={{ 
+        fontWeight: 'bold', 
+        color: 'white', 
+        background: 'red', 
+      }}>Trending</NavLink>
+      <NavLink to='/myNews' className={styles.link} activeStyle={{ 
+        fontWeight: 'bold', 
+        color: 'white', 
+        background: 'red', 
+      }}>My News</NavLink>
     </span>
   );
 }
