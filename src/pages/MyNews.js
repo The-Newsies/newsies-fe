321374fe8b442d5eddd57333edaf0863faf2803b
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-17 15:21:17 -0700

    we plugged the title component into each page view and passed it a title

--- src/pages/MyNews.js
+++ src/pages/MyNews.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import Header from '../components/header/Header';
 import PropTypes from 'prop-types';
+import PageTitle from '../components/pageTitle/PageTitle';
 
 export default function MyNews({ location }) {
   return (
     <>
       <Header location={location}/>
+      <PageTitle title='My News' />
     </>
   );
 }
