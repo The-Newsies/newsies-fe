10a7e4fdcd912331f6da89d04c49748c0f99b081
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-09-17 13:22:06 -0700

    removed search bar from trending page

--- src/components/header/MyViewWrapper.js
+++ src/components/header/MyViewWrapper.js
@@ -1,26 +1,11 @@
 import React from 'react';
-import SearchBarContainer from '../../containers/SearchBarContainer';
 import Avatar from '../avatar/Avatar';
 import DropDownMenu from '../dropDownMenu/DropDownMenu';
 import styles from './MyViewWrapper.css';
-import PropTypes from 'prop-types';
 
-export default function MyViewWrapper({ location }) {
-  if(location.pathname === '/myNews') {
-    return (
-      <div className={styles.container}>
-        <div className={styles.avatarSignOut}>
-          <Avatar />
-          <DropDownMenu />
-        </div>
-      </div>
-    );
-  }
+export default function MyViewWrapper() {
   return (
     <div className={styles.container}>
-      <div>
-        <SearchBarContainer />
-      </div>
       <div className={styles.avatarSignOut}>
         <Avatar />
         <DropDownMenu />
@@ -28,7 +13,3 @@ export default function MyViewWrapper({ location }) {
     </div>
   );
 }
-
-MyViewWrapper.propTypes = {
-  location: PropTypes.object.isRequired
-};
