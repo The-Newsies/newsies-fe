13f60571773ed75f69f8ad4b0bdd7ed2903ab108
sepuckett86 <sepuckett86@gmail.com>
2019-09-18 12:45:59 -0700

    created a selector for our collections and wrote a test for it

---
+++ src/selectors/collectionsSelector.test.js
@@ -0,0 +1,21 @@
+import { getUserCollections } from './collectionsSelector';
+
+describe('collectionsSelector tests', () => {
+  it('gets the collections from state', () => {
+    const state = {
+      collections: [{ _id: null,
+        name: 'Nypost.com',
+        description: 'today is weds',
+        articleIds: []
+      }]
+    };
+    console.log(collections)
+    const collections = getUserCollections(state);
+
+    expect(collections).toEqual([{ _id: null,
+      name: 'Nypost.com',
+      description: 'today is weds',
+      articleIds: []
+    }]);
+  });
+});
