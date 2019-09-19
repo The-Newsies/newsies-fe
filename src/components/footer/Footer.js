323ffb3079b22aecea66f97fea31eab3a79ecae8
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-09-19 09:31:00 -0700

    basic footer

---
+++ src/components/footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import styles from './Footer.css';
+
+export default function Footer() {
+  return (
+    <div>
+      <span>Newzie created by <a href="https://github.com/aramirezdecruz3148">A. Ramirez de Cruz</a>, <a href="https://github.com/carlosus">C. Santiago</a>, <a href="https://github.com/lacrivella">LA. Crivella</a>, and <a href="https://github.com/sepuckett86">S. Puckett</a></span>
+      <p>special thanks to the <a href="https://newsapi.org/">News API</a></p>
+    </div>
+  );
+}
