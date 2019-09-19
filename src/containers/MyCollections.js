2af7213addf0730cd960f14321563338bc590ab5
aramirezdecruz3148 <leigh3148@gmail.com>
2019-09-18 18:24:13 -0700

    rendered the container inside of MyNews page view and have collections coming through

---
+++ src/containers/MyCollections.js
@@ -0,0 +1,44 @@
+import { connect } from 'react-redux';
+import React, { Component } from 'react';
+import PropTypes from 'prop-types';
+import CollectionList from '../components/collections/CollectionList';
+import { fetchCollections } from '../actions/collectionsActions';
+import { getUserCollections } from '../selectors/collectionsSelector';
+
+
+class MyCollections extends Component {
+  static propTypes = {
+    collections: PropTypes.arrayOf(PropTypes.shape({
+      name: PropTypes.string.isRequired,
+      description: PropTypes.string.isRequired,
+      _id: PropTypes.string.isRequired    
+    })).isRequired,
+    fetch: PropTypes.func.isRequired
+  }
+
+  componentDidMount() {
+    this.props.fetch();
+  }
+
+  render() {
+    const { collections } = this.props;
+    return (
+      <CollectionList collections={collections} />
+    );
+  }
+}
+
+const mapStateToProps = state => ({
+  collections: getUserCollections(state)
+});
+
+const mapDispatchToProps = dispatch => ({
+  fetch() {
+    dispatch(fetchCollections());
+  }
+});
+
+export default connect(
+  mapStateToProps, 
+  mapDispatchToProps
+)(MyCollections);
--- src/pages/MyNews.js
+++ src/pages/MyNews.js
@@ -2,12 +2,14 @@ import React from 'react';
 import Header from '../components/header/Header';
 import PropTypes from 'prop-types';
 import PageTitle from '../components/pageTitle/PageTitle';
+import MyCollections from '../containers/MyCollections';
 
 export default function MyNews({ location }) {
   return (
     <>
       <Header location={location}/>
       <PageTitle title='My News' />
+      <MyCollections />
     </>
   );
 }
