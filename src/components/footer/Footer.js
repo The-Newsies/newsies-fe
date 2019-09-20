1177a412feb3adf403cfabb8f302e6f88d19ea73
Leigh-Ann Crivella <la.crivella.2@gmail.com>
2019-09-19 19:16:20 -0700

    made footer fix

--- src/components/footer/Footer.js
+++ src/components/footer/Footer.js
@@ -4,7 +4,7 @@ import styles from './Footer.css';
 export default function Footer() {
   return (
     <div className={styles.body}>
-      <span className={styles.container}>Newzie created by <a href="https://github.com/aramirezdecruz3148">A. Ramirez de Cruz</a> <a href="https://github.com/carlosus">C. Santiago</a> <a href="https://github.com/lacrivella">LA. Crivella</a> <a href="https://github.com/sepuckett86">S. Puckett</a></span>
+      <span className={styles.container}>Newzie created by <a href="https://github.com/aramirezdecruz3148">A. Ramirez de Cruz</a> <a href="https://github.com/carlosus">C. Us Santiago</a> <a href="https://github.com/lacrivella">LA. Crivella</a> <a href="https://github.com/sepuckett86">S. Puckett</a></span>
       <p className={styles.container}>Powered by the<a href="https://newsapi.org/">News API</a></p>
     </div>
   );
