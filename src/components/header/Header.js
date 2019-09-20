8eaa19612472131ddcef7f603fc0d761243a4b76
sepuckett86 <sepuckett86@gmail.com>
2019-09-19 19:35:13 -0700

    add link to logo

--- src/components/header/Header.js
+++ src/components/header/Header.js
@@ -5,6 +5,7 @@ import MyViewWrapper from './MyViewWrapper';
 import styles from './Header.css';
 import logo from '../../assets/logo.png';
 import PropTypes from 'prop-types';
+import { Link } from 'react-router-dom';
 
 export default function Header({ location }) {
   const { isAuthenticated } = useAuth0();
@@ -12,7 +13,9 @@ export default function Header({ location }) {
   return (
     <>
     <div className={styles.header}>
-      <img className={styles.logo} src={logo} />
+      <Link to={'/'}>
+        <img className={styles.logo} src={logo} alt="Newzie_logo"/>
+      </Link>
       {!isAuthenticated ? <LandingWrapper /> : <MyViewWrapper location={location} />}
     </div>
     </>
